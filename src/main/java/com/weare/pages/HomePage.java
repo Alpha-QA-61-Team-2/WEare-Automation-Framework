ba07ba26121b1803beaaaf22f175129bc474b992
olgaatanasova <olga.atanasova.a61@learn.telerikacademy.com>
2024-11-22 13:10:14 +0200

    editing AddAndApproveFriendRequestTest in order to send friend request every time to the same user;

--- src/main/java/com/weare/pages/HomePage.java
+++ src/main/java/com/weare/pages/HomePage.java
@@ -35,6 +35,11 @@ public class HomePage extends BaseHeader {
         js.executeScript("window.scrollBy(0, 2500);");
         driver().findElement(userProfileBtn).click();
     }
+    public void goToUserPage() {
+        String url = "http://localhost:8081/auth/users/43/profile";
+        driver().get(url);
+    }
+
     //todo remove below methods since they exist in LoggedHeader
     public void selectPersonalProfile() {
         driver().findElement(personalProfileBtn).click();
